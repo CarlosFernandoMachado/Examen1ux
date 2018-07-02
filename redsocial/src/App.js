feceedf47afa3f7deeb0145210fe29020673470b
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2018-07-01 18:27:01 -0600

    creando los mensaje

--- redsocial/src/App.js
+++ redsocial/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Note from './Note/Note'
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      notes: [
+        {id: 1, noteContent: "Note 1"},
+        {id: 2, noteContent: "Note 2"},
+      ],
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="notesWrapper">
+        <h1>Red Social</h1>
+        <Note noteContent ={} noteId={} key={}/>
       </div>
     );
   }
