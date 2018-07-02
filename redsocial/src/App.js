0c5256b1d92804b46d88162069cb487d9d083d55
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2018-07-01 20:48:02 -0600

    solo funcionan los mensajes

--- redsocial/src/App.js
+++ redsocial/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
-import Note from './Note/Note'
-import NoteForm from './NoteForm/NoteForm'
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
 import { DB_CONFIG } from './Config/config';
 import firebase from 'firebase/app';
 import 'firebase/database';
-
+import './App.css';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.addNote = this.addNote.bind(this);
-    this.App = firebase.initializeApp(DB_CONFIG);
-    this.db = this.App.database().ref().child('notes');
+    this.removeNote = this.removeNote.bind(this);
+
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('notes');
+
+    // We're going to setup the React state of our component
     this.state = {
       notes: [],
     }
@@ -20,11 +24,26 @@ class App extends Component {
 
   componentWillMount() {
     const previousNotes = this.state.notes;
+
+    // DataSnapshot
     this.database.on('child_added', snap => {
       previousNotes.push({
         id: snap.key,
         noteContent: snap.val().noteContent,
       })
+
+      this.setState({
+        notes: previousNotes
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for (var i = 0; i < previousNotes.length; i++) {
+        if (previousNotes[i].id === snap.key) {
+          previousNotes.splice(i, 1);
+        }
+      }
+
       this.setState({
         notes: previousNotes
       })
@@ -32,19 +51,30 @@ class App extends Component {
   }
 
   addNote(note) {
-    this.database.push().set({ noteContent: note});
+    this.database.push().set({ noteContent: note });
   }
+
+  removeNote(noteId) {
+    console.log("from the parent: " + noteId);
+    this.database.child(noteId).remove();
+  }
+
   render() {
     return (
       <div className="notesWrapper">
         <div className="notesHeader">
-          <div className="heading"> Red Social</div>
+          <div className="heading">
+            Red Social
+          </div>
         </div>
         <div className="notesBody">
           {
             this.state.notes.map((note) => {
               return (
-                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                <Note noteContent={note.noteContent}
+                  noteId={note.id}
+                  key={note.id}
+                  removeNote={this.removeNote} />
               )
             })
           }
--- redsocial/src/Note/Note.jsx
+++ redsocial/src/Note/Note.jsx
@@ -1,23 +1,35 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import './Note.css';
 import PropTypes from 'prop-types';
 
 class Note extends Component{
+
     constructor(props){
         super(props);
-        this.noteContent = props.noteContent;
-        this.noteId = props.noteId;
+        this.noteContent = props.noteContent; 
+        this.noteId = props.noteId; 
+        this.handleRemoveNote = this.handleRemoveNote.bind(this);
     }
 
-    render(props){
+    handleRemoveNote(id){
+        this.props.removeNote(id);
+    }
+
+    render(){
         return(
             <div className="note fade-in">
-                <p className="noteContent">{this.noteContent}</p>
+                <span className="closebtn" 
+                      onClick={() => this.handleRemoveNote(this.noteId)}>
+                      &times;
+                </span>
+                <p className="noteContent">{ this.noteContent }</p>
             </div>
         )
     }
 }
-Note.PropTypes = {
+
+Note.propTypes = {
     noteContent: PropTypes.string
 }
+
 export default Note;
