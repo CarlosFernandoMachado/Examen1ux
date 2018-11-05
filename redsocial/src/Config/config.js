ce95cd48633b893d7bab78b322fa52b17ebb9971
CarlosFernandoMachado <testsubjectno2@hotmail.com>
2018-11-05 07:27:38 -0600

    Update redsocial/src/Config/config.js

--- redsocial/src/Config/config.js
+++ redsocial/src/Config/config.js
@@ -8,3 +8,14 @@
     storageBucket: "examen-ux-535ed.appspot.com",
     messagingSenderId: "143702404121"
   };
+/*
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8dKYoertEFlqVYTUZs-dzLSy9D_ejX54",
+    authDomain: "examen-ux-535ed.firebaseapp.com",
+    databaseURL: "https://examen-ux-535ed.firebaseio.com",
+    projectId: "examen-ux-535ed",
+    storageBucket: "examen-ux-535ed.appspot.com",
+    messagingSenderId: "143702404121"
+  };
+  firebase.initializeApp(config);*/
